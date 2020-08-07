0433c3f48cd36195430e7ddb0950965bed033d1a
Blue-Silence <Blue-Silence@users.noreply.github.com>
2020-08-08 07:43:28 +0800

    Sys

---
+++ Sys.hs
@@ -0,0 +1,30 @@
+module Sys(
+    syscall
+   ,fList
+   ,nameList
+   ,isSyscall
+)where
+
+import Type
+
+
+syscall :: Val->[Val]->Val
+
+syscall (Op n) x = (match n fList) x
+
+fList ::[(Not,([Val]->Val))] --syscall名称与实际函数对应关系(由实现决定)
+nameList ::[String] --syscall名称(由实现决定)
+
+isSyscall x = elem x nameList 
+
+match n ((id,f):idfs)
+    |n==id = f 
+    |otherwise = match n idfs
+
+
+-----------------------------------------------------------------------------------------
+--Where the magic happens
+
+fList = []
+
+nameList = []
--- Type.hs
+++ Type.hs
@@ -10,12 +10,17 @@ data Term = Terms Term [Term] ENV
            |Termf Val [Term] ENV 
            |TermVal Val 
            |TermVar Not ENV 
-
+            deriving Show
+            
 data Val = Num Int
           |Op Not
           |Lambda [Not] Term 
-        
+            deriving Show
+
 type Not = [Char]
+    
 type ENV = [[Def]]
+
 data Def = Def Not Term 
           |ClosedDef Not Term
+            deriving Show
