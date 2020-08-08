5cd89f3f433fe1b3041c9b97687d7973f5447c48
Blue-Silence <Blue-Silence@users.noreply.github.com>
2020-08-08 09:40:20 +0800

    ~

--- Type.hs
+++ Type.hs
@@ -4,22 +4,30 @@ module Type (
    ,Not
    ,ENV
    ,Def(..)
+   ,emptyENV
 ) where
+
 
 data Term = Terms Term [Term] ENV 
            |Termf Val [Term] ENV 
            |TermVal Val 
            |TermVar Not ENV 
             deriving Show
-            
+
+
 data Val = Num Int
           |Op Not
           |Lambda [Not] Term 
             deriving Show
 
+
 type Not = [Char]
-    
+
+
 type ENV = [[Def]]
+
+emptyENV=[[]]
+
 
 data Def = Def Not Term 
           |ClosedDef Not Term
