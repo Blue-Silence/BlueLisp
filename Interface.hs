fc4422084045049d1ece1080d4db42a3bccd387a
Blue-Silence <Blue-Silence@users.noreply.github.com>
2020-08-08 11:04:30 +0800

    Interface

---
+++ Interface.hs
@@ -0,0 +1,17 @@
+module Interface(interpreter)where
+
+import Type
+import Lexer
+import Eval 
+
+interpreter :: String->Val
+
+interpreter=eval . term_gen . words . preProcess
+
+
+
+
+
+preProcess :: String->String --在 '(' , ')' 周围加上空格
+preProcess []=[]
+preProcess (x:xs) = if ((x=='(')||(x==')')) then ' ':x:' ':(preProcess xs) else x:(preProcess xs)
